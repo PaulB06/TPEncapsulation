c7fc716e030a14b845b5d544965fd3284930c037
Paul BIGOT <paulbigot06@gmail.com>
2025-02-03 12:26:36 +0100

    commit tp3 fini

--- src/fr/sdv/bank/TestBank.java
+++ src/fr/sdv/bank/TestBank.java
@@ -1,10 +1,13 @@
 package fr.sdv.bank;
 
 import fr.sdv.bank.entities.Compte;
+import fr.sdv.bank.entities.CompteTaux;
 
 public class TestBank {
     public static void main(String[] args) {
         Compte monCompte2 = new Compte(2, 851.3);
         System.out.println(monCompte2.toString());
+        CompteTaux monCompte3 = new CompteTaux(2, 851.3, 7);
+        System.out.println(monCompte3.toString());
     }
 }
---
+++ src/fr/sdv/bank/entities/CompteTaux.java
@@ -0,0 +1,25 @@
+package fr.sdv.bank.entities;
+
+public class CompteTaux extends Compte {
+    private double taux;
+
+    public CompteTaux(int numeroCompte, double solde, double taux) {
+        super(numeroCompte, solde);
+        this.taux = taux;
+    }
+
+    public double getTaux() {
+        return taux;
+    }
+
+    public void setTaux(double taux) {
+        this.taux = taux;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "  " +
+                "taux=" + taux + " %";
+
+    }
+}
